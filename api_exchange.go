0fe16992fe58fe1ade9b9ec65182c57a725770b5
tomme87 <tomrf@stud.ntnu.no>
2017-10-24 13:01:21 +0200

    testing

--- api_exchange.go
+++ api_exchange.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+)
 
 func handleRootPath(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hei1")
+}
 
+func handleRootPath2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hei2")
+}
+
+func handleRootPath3(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hei3")
 }
--- main.go
+++ main.go
@@ -8,4 +8,7 @@ const (
 
 func main() {
 	http.HandleFunc(BasePath, handleRootPath)
+	http.HandleFunc(BasePath + "/", handleRootPath2)
+	http.HandleFunc(BasePath + "/lol", handleRootPath3)
+	http.ListenAndServe(":8080", nil)
 }
