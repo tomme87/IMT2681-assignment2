8002fcb36c055f14f4749f148dc816210c9cce50
tomme87 <tomrf@stud.ntnu.no>
2017-10-31 22:57:35 +0100

    fix monitor

--- monitor/main.go
+++ monitor/main.go
@@ -5,31 +5,35 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/tomme87/IMT2681-assignment2/api"
 	"os"
+	"strings"
 )
 
 func updateTicker() {
 	fmt.Println("Fetching new exchange rates.")
 	fixer, err := api.NewFixer()
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Printf("Error fetching rates: %s\n", err.Error())
 		return
 	}
 
 	err = api.Db.AddCurrency(*fixer)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		fmt.Printf("Error adding Currency to db: %s\n", err.Error())
+		if !strings.HasPrefix(err.Error(), "E11000") { // Return unless it's a duplicate key error.
+			return
+		}
 	}
 
 	webhooks := api.Db.GetAll()
 	for _, wh := range webhooks {
 		cRate, err := fixer.GetRate(wh.BaseCurrency, wh.TargetCurrency)
 		if err != nil {
-			fmt.Printf("Error getting rate for %s: %s", wh.ID.Hex(), err.Error())
+			fmt.Printf("Error getting rate for %s: %s\n", wh.ID.Hex(), err.Error())
 			continue
 		}
 
 		if cRate < wh.MinTriggerValue || cRate > wh.MaxTriggerValue {
+			fmt.Printf("Invoking %s", wh.WebhookURL)
 			wh.CurrentRate = cRate
 			err := wh.Invoke()
 			if err != nil {
@@ -38,6 +42,7 @@ func updateTicker() {
 			}
 		}
 	}
+	fmt.Println("done")
 }
 
 
