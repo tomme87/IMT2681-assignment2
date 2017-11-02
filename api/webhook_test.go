82d5015142a1f6a8cc5782d74796f7e842ba28d7
tomme87 <tomrf@stud.ntnu.no>
2017-11-02 03:18:01 +0100

    more testing on evalutationtrigger

--- api/webhook_test.go
+++ api/webhook_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"net/http"
 	"gopkg.in/mgo.v2/bson"
+	"encoding/json"
 )
 
 // TestWebhook_Validate tests the validate function
@@ -29,6 +30,17 @@ func TestWebhook_Invoke(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			http.Error(w, "Only POST", http.StatusBadRequest)
+		} else {
+			wh := Webhook{}
+			err := json.NewDecoder(r.Body).Decode(&wh)
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+			if wh.CurrentRate != float32(9.5348) {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
 		}
 	}))
 	defer ts.Close()
@@ -37,6 +49,7 @@ func TestWebhook_Invoke(t *testing.T) {
 		WebhookURL: ts.URL,
 		BaseCurrency: "USD",
 		TargetCurrency: "NOK",
+		CurrentRate: 9.5348,
 		MinTriggerValue: 0.2,
 		MaxTriggerValue: 1.3,
 	}
